cab25b22fabcbc5e78a39d58b809f8b5b576f553
Tuan <anhtuan932003@gmail.com>
2022-04-17 13:24:20 +0700

    message

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'À há.Cậu nghĩ lấy được fb tớ dễ thế sao? Còn lâu nha!!',
+    introDesc: `Để có được fb tớ. Cậu trả lời câu hỏi này đã nha.`,
+    btnIntro: 'OK nhé!',
+    title: 'Cậu thích tớ có phải không nào ._. ? 😙',
+    desc: 'Nếu cậu không trả lời hoặc thoát ra, tức là cậu là người yêu tớ rồi đó nha :> ',
+    btnYes: 'Thích ơi là thíchh 💖💖💖',
+    btnNo: 'Cậu mơ à :333 💔 ',
+    question:'Nói cho tớ biết đi vì sao cậu thích tớ zậyyy 😊😊',
+    reasonPlaceholder: 'Whyyy',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Vì cậu đẹp trai lắm lunn á :333',
+    mess: 'Tớ biết mà, Tớ cũng thích cậu lắm lun!! Công chúa nhỏ của tớ ạ. Love you 300.000 😘😘',
+    messDesc: '💬 Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo. 💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.messenger.com/t/100035614126252'
+}
